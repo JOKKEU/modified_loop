3d0074d48a01500409baf40362dd4d1e1a33335b
.Replicant~ <102823475+JOKKEU@users.noreply.github.com>
2025-10-17 05:58:34 +0300

    check_crypt.c

--- check_crypt.c
+++ check_crypt.c
@@ -2,17 +2,11 @@
 
 int check_encrypt_part(char* buffer_zero_sector, unsigned offset, size_t size)
 {
-
-	max_loop = 7;
-	is_check_disk = false;
-	CIPHER_KEY = 0xAA;
-	control_part_crypt.flag = false; // Изначально думаем что диск не шифрован
-	LOG(KERN_INFO, "\n");
 	int is_encrypt = 1; // не шифрован
 	int i = 0;
 	while(!is_encrypt || i <= size)
 	{
-		if (buffer_zero_sector[i] != 0x00) {is_encrypt = 0; break; LOG(KERN_INFO, "\n");} // раздел шифрован
+		if (buffer_zero_sector[i] != 0x00) {is_encrypt = 0; break;} // раздел шифрован
 		++i;
 	}
 
